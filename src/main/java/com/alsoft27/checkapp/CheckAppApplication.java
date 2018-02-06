6b2c143e53fb9276f8ec05bd7c53c9ca8c6d2b38
alsoft27 <alsoft27@gmail.com>
2018-02-06 20:35:46 +0100

    first commit

---
+++ src/main/java/com/alsoft27/checkapp/CheckAppApplication.java
@@ -0,0 +1,12 @@
+package com.alsoft27.checkapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckAppApplication.class, args);
+	}
+}
